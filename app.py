486a460a358b8a7270456e131c642fcd5c470bac
attiliogiglio <attilio.g93@gmail.com>
2020-09-30 15:48:36 -0300

    post and put method

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from src.products import Products
 
 app = Flask(__name__)
@@ -16,6 +16,34 @@ def get_products():
 @app.route('/products/<string:product_name>')
 def get_product(product_name):
     products_found = [product for product in Products if product['name'] == product_name]
-    return jsonify({'product':products_found[0]})
+    if (len(products_found) > 0): 
+        return jsonify({'product':products_found[0]})
+    else:
+        return jsonify({'message':'Product not found'})
+
+@app.route('/products', methods=['POST'])
+def post_products():
+    new_product = {
+        'name': request.json['names'],
+        'price': request.json['prices'],
+        'quantity': request.json['quantities']
+    }
+    Products.append(new_product)
+    return ({'message':'Products add amazing', "products": Products})
+
+@app.route('/products/<string:product_name>', methods:['PUT'])
+def edit_product(product_name): 
+    products_found = [product for product in Products if product['name'] == product_name]
+    if (len(products_found) > 0): 
+        products_found[0]['name'] = request.json['name']
+        product_found[0]['price'] = request.json['price']
+        product_found[0]['quantity'] = request.json['quantity']
+        return jsonify({
+            'message':'Product updated',
+            'product':products_found[0]
+        }) 
+    else:
+        return jsonify({'message':'Not found boy'})
+
 
 app.run(host='127.0.0.1', port=4000, debug=True)
