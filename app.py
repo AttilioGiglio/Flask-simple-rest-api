be662ac3d9794b61d7c9836784d2fa62ec9fd575
attiliogiglio <attilio.g93@gmail.com>
2020-09-30 13:14:06 -0300

    transform code to json and send it to the view as json

--- app.py
+++ app.py
@@ -1,10 +1,21 @@
-from flask import Flask
+from flask import Flask, jsonify
 from src.products import Products
 
 app = Flask(__name__)
 
+# Method GET is the default method, not need to add
 @app.route('/')
 def start():
-    return 'Estamos on compare, ahora si!!!'
+    return jsonify({"mesage": "ping-pong"}) 
+
+@app.route('/products', methods=['GET'])
+def get_products():
+    # return jsonify(Products)
+    return jsonify({'products':Products, 'message': 'Products List'})
+
+@app.route('/products/<string:product_name>')
+def get_product(product_name):
+    products_found = [product for product in Products if product['name'] == product_name]
+    return jsonify({'product':products_found[0]})
 
 app.run(host='127.0.0.1', port=4000, debug=True)
--- src/products.py
+++ src/products.py
@@ -1,5 +1,5 @@
 Products = [
     {'name':'laptop', 'price':800, 'quantity':4},
-    {'name':'laptop', 'price':40, 'quantity':10},
-    {'name':'laptop', 'price':400, 'quantity':3}
+    {'name':'keyboard', 'price':40, 'quantity':10},
+    {'name':'speaker', 'price':400, 'quantity':3}
 ]
